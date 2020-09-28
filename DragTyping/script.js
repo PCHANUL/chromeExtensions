64dfdc072e0ac6cb5efe2231629639f22ca065a7
chanul94 <cksdnfwkd@naver.com>
2020-09-28 17:13:55 +0900

    Implement spacebar

--- DragTyping/script.js
+++ DragTyping/script.js
@@ -1,6 +1,6 @@
 // initial setting
 let input = '';
-let keys = [8,9,13,16,17,18,91,32,20,39,38,37,40,93];
+let keys = [8,9,13,16,17,18,91,20,39,38,37,40,93];
 
 let letters = {
   arr: [],
@@ -19,6 +19,7 @@ chrome.tabs.executeScript({
     printLetter('init')
   } catch (err) {
     console.log(err)
+    printLetter('init')
   }
 });
 
@@ -41,26 +42,29 @@ document.addEventListener('keydown', (e) => {
     input = input + e.key
   }
   
-  // print input letter
-    
+  // 한글을 초중종성 결합
   let printInputLetter = Hangul.assemble(input.split(''))
+
+  // 입력된 값이 현재 출력된 값과 같다면 다음 글자로 넘어갑니다.
   if (printInputLetter === letters.arr[letters.index]) {
     setTimeout(() => {
       input = '';
       letters.index ++;
       printLetter();
       document.getElementById("input").innerHTML = input
-    }, 500)
+    }, 0)
+  } else if (printInputLetter !== letters.arr[letters.index]) {
+    document.getElementById("input").innerHTML = printInputLetter;
   }
-
-  document.getElementById("input").innerHTML = printInputLetter;
-
 });
 
 function printLetter(isInit) {
   if (!letters.arr[letters.index]) {
-    document.getElementById("letter").innerHTML 
+    document.getElementById("selected").innerHTML 
       = isInit ? '연습할 문장을 드래그하세요' : '모든 텍스트를 타이핑하였습니다.'; 
+    document.getElementById("letter").innerHTML = '';
+  } else if (letters.arr[letters.index] === ' ') {
+    document.getElementById("letter").innerHTML = "<img src='/img/spacebar.png' width='50' height='50'>";
   } else {
     document.getElementById("letter").innerHTML = letters.arr[letters.index];
   }
