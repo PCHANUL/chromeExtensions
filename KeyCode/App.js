9421a105a1967761aec886b77ac96e18ab0b321f
chanul94 <cksdnfwkd@naver.com>
2020-09-30 22:32:14 +0900

    Merge origin master

--- KeyCode/App.js
+++ KeyCode/App.js
@@ -4,12 +4,18 @@ class App {
     
     // 키를 입력하면 화면에 출력
     document.addEventListener('keydown', (event) => {
-      console.log($target)
+      console.log(event)
       this.input = {
         keyCode: event.keyCode,
         code: event.code,
         key: event.key,
-        location: event.location,
+        location: event.location === 0 ? (
+          '0 (General Keys)' 
+        ) : event.location === 1 ? (
+          '1 (Left-side modifier keys)'
+        ) : (
+          '2 (Right-side modifier keys)'
+        ),
       }
       this.render({ $target });
     });
@@ -22,6 +28,9 @@ class App {
     console.log('render');
     $target.innerHTML = `
     <p id='keyCode'>${this.input.keyCode}</p>
+    <p id='code'>${this.input.code}</p>
+    <p id='key'>${this.input.key}</p>
+    <p id='location'>${this.input.location}</p>
     `;
   }
 }
