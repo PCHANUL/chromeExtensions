5709347c8ea2b05192130baa55a9d6f95d355560
chanul94 <cksdnfwkd@naver.com>
2020-11-22 22:05:52 +0900

    Implement calculator function

--- CodeSpeed/App.js
+++ CodeSpeed/App.js
@@ -1,13 +1,80 @@
+
+
 class App {
   constructor($target) {
     this.target = $target;
 
     this.render();
+    this.getData();
+  }
+
+  getData() {
+    chrome.storage.sync.get((data) => {
+      if (data.code) document.querySelector('#codeArea').value = data.code;
+    })
+  }
+  
+  updateData(e) {
+    chrome.storage.sync.set({
+      code: e.target.value
+    });
   }
 
   render() {
-    let div = document.createElement('div');
-    div.innerText = 'hello'
-    this.target.appendChild(div)
+    // textarea
+    let codeArea = document.createElement('textarea');
+    codeArea.id = 'codeArea';
+    codeArea.className = 'form-control';
+    codeArea.addEventListener('keyup', this.updateData);
+    this.target.appendChild(codeArea);
+
+    // input button
+    let inputButton = document.createElement('button');
+    inputButton.className = "btn btn-primary";
+    inputButton.innerText = '성능측정'
+    inputButton.addEventListener('click', getCode)
+    this.target.appendChild(inputButton);
+    
+    // init button
+    let initButton = document.createElement('button');
+    initButton.className = "btn btn-outline-secondary";
+    initButton.innerText = '초기화'
+    initButton.addEventListener('click', initCode)
+    this.target.appendChild(initButton);
+
+    // result div
+    let resultDiv = document.createElement('div');
+    resultDiv.id = 'result';
+    resultDiv.style.border = '1px solid #000';
+    this.target.appendChild(resultDiv);
   }
 }
+
+function initCode() {
+  document.querySelector('#codeArea').value = '';
+  chrome.storage.sync.set({
+    code: ''
+  });
+}
+
+function getCode() {
+  let target = document.querySelector('#result');
+  target.innerHTML = '';
+  let code = document.querySelector('#codeArea').value;
+  for(let i = 0; i < 30; i++) {
+    target.insertAdjacentHTML('beforeend',`코드 속도: ${calcFunction(code)}<br />`);
+  }
+}
+
+function calcFunction(code) {
+  let start = performance.now();
+  for(let i = 0; i < 30; i++) {
+  new Function(code)();
+  }
+  let finished = performance.now();
+
+  elapsed = (finished - start);
+
+  return elapsed;
+}
+
