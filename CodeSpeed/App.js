53faf8ba4fa62f1c0aa7a4f4731e095bfbc86b0d
chanul94 <cksdnfwkd@naver.com>
2020-11-23 02:08:05 +0900

    Implement draw graph

--- CodeSpeed/App.js
+++ CodeSpeed/App.js
@@ -1,9 +1,20 @@
 class App {
   constructor($target) {
     this.target = $target;
+    this.dataArr = [];
+
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
 
     this.render();
     this.getData();
+  }
+
+  initCode() {
+    document.querySelector('#codeArea').value = '';
+    chrome.storage.sync.set({
+      code: ''
+    });
   }
 
   getData() {
@@ -18,86 +29,67 @@ class App {
     });
   }
 
+  getCode() {
+    this.dataArr = [];
+    let code = document.querySelector('#codeArea').value;
+    for(let i = 0; i < 5; i++) {
+      let result = this.calcFunction(code);
+      this.dataArr.push(result);
+    }
+    this.drawGraph();
+  }
+
+  drawGraph() {
+    let clientWidth = this.canvas.width;
+    let clientHeight = this.canvas.height;
+    let gWidth = clientWidth / 10;
+    
+    this.dataArr.forEach((data, idx) => {
+      this.ctx.fillStyle = '#4287f5';
+      this.ctx.fillRect((gWidth + 5) * idx, clientHeight, gWidth, -1 * data);
+    })
+  }
+  
+  calcFunction(code) {
+    let start = performance.now();
+    for(let i = 0; i < 30; i++) {
+      new Function(code)();
+    }
+    let finished = performance.now();
+    let elapsed = (finished - start);
+    return elapsed;
+  }
+
+
   render() {
     // textarea
     let codeArea = document.createElement('textarea');
     codeArea.id = 'codeArea';
     codeArea.className = 'form-control';
-    codeArea.addEventListener('keyup', this.updateData);
+    codeArea.addEventListener('keyup', this.updateData.bind(this));
     this.target.appendChild(codeArea);
 
     // input button
     let inputButton = document.createElement('button');
     inputButton.className = "btn btn-primary";
     inputButton.innerText = '성능측정'
-    inputButton.addEventListener('click', getCode)
+    inputButton.addEventListener('click', this.getCode.bind(this));
     this.target.appendChild(inputButton);
     
     // init button
     let initButton = document.createElement('button');
     initButton.className = "btn btn-outline-secondary";
     initButton.innerText = '초기화'
-    initButton.addEventListener('click', initCode)
+    initButton.addEventListener('click', this.initCode.bind(this))
     this.target.appendChild(initButton);
 
-    // result div
-    let resultDiv = document.createElement('div');
-    resultDiv.id = 'result';
-    resultDiv.style.border = '1px solid #000';
-    this.target.appendChild(resultDiv);
-
-
-    let canvas = document.createElement('canvas');
-    canvas.id = 'graphCanvas';
-    let ctx = canvas.getContext('2d');
-    this.target.appendChild(canvas);
-
-    drawGraph(ctx);
-
-    
+    // graph canvas   
+    this.canvas.id = 'graphCanvas';
+    this.target.appendChild(this.canvas);
   }
 }
 
-function drawGraph(ctx) {
-  
 
-  let clientWidth = document.querySelector('#graphCanvas').clientWidth;
-  let clientHeight = document.querySelector('#graphCanvas').clientHeight;
-  alert(clientHeight);
 
-  let gWidth = clientWidth / 10;
-  ctx.fillStyle = '#000';
-  ctx.fillRect(gWidth, clientHeight, 50, -50);
-  
 
-  
-}
-
-function initCode() {
-  document.querySelector('#codeArea').value = '';
-  chrome.storage.sync.set({
-    code: ''
-  });
-}
-
-function getCode() {
-  let target = document.querySelector('#result');
-  target.innerHTML = '';
-  let code = document.querySelector('#codeArea').value;
-  for(let i = 0; i < 30; i++) {
-    target.insertAdjacentHTML('beforeend',`코드 속도: ${calcFunction(code)}<br />`);
-  }
-}
-
-function calcFunction(code) {
-  let start = performance.now();
-  for(let i = 0; i < 30; i++) {
-  new Function(code)();
-  }
-  let finished = performance.now();
-
-  elapsed = (finished - start);
-
-  return elapsed;
-}
 
